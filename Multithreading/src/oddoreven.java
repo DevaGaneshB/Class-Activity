328150ce21600cb62dde91ff93889651c1971ca4
DevaGaneshB <40600219+DevaGaneshB@users.noreply.github.com>
2018-08-12 16:19:17 +0530

    Add files via upload

--- Multithreading/src/oddoreven.java
+++ Multithreading/src/oddoreven.java
@@ -1,70 +1,72 @@
-
 import java.io.*;  
+class Table {
+	
+			 synchronized String printTable(int n){//synchronized method  
+				  
+		    	 String s="";
+			     try{
+							 for(int i=n;i<=10;i=i+2){
+								 s=s+"\t"+i;
+							 }  
 
-	 class Table {
+					  
+			      Thread.sleep(400);  
+		     }catch(Exception e){System.out.println(e);} 
+			     System.out.println("Sucess...");
+			     return s;
+					  
+			 }  
+			 }  
+			  
+			class MyThread1 extends Thread{  
+			Table t;  
+			MyThread1(Table t){  
+			this.t=t;  
+			}  
+			public void run() {  
+				PrintWriter fout1;
+				try {
+					fout1 = new PrintWriter("F:\\Even.txt");
+					t.printTable(1);
+					fout1.write(t.printTable(0));
+					fout1.close();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
 			
-		 synchronized void printTable(int n,String File) throws FileNotFoundException{//synchronized method  
-		
-			 
-		   String s="";
-		   PrintWriter fout=new PrintWriter(File);        
-		     
-		   for(int i=n;i<=10;i=i+2){
-			   s=s+"\t"+i;
-		   }   
-		   fout.write(s);  
-		   fout.close();
-		   System.out.println("success...");
-		     try{  
-		      Thread.sleep(1400);  
-	     }catch(Exception e){System.out.println(e);}  
-		     
-		  
-		 }  
 		}  
-		  
-		class MyThread1 extends Thread{  
+			  
+			}  
+			class MyThread2 extends Thread{  
 		Table t;  
-		MyThread1(Table t){  
-		this.t=t;  
-		}  
-		public void run(){
-		try {
-			String File="Even.txt";
-			
-			t.printTable(0,File);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  
-	}  
-		  
-		}  
-		class MyThread2 extends Thread{  
-	Table t;  
-		MyThread2(Table t){  
-		this.t=t;  
-		}  
-		public void run(){  
-		try {
-			String fil="‪Odd.txt";
-			t.printTable(1,fil);
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  
-		}  
-		}  
-		  
-		public class oddoreven{ 
-	public static void main(String args[]){  
-		Table obj = new Table();//only one object  
-		MyThread1 t1=new MyThread1(obj);  
-		MyThread2 t2=new MyThread2(obj);  
-		t1.start();  
-		t2.start();  
-	}  
-  
+			MyThread2(Table t){  
+			this.t=t;  
+			}  
+			public void run(){  
 
+				PrintWriter fout2;
+				try {
+					fout2 = new PrintWriter("E:\\Odd.txt");
+					t.printTable(1);
+					fout2.write(t.printTable(1));
+					fout2.close();
+				} catch (FileNotFoundException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			
+			}  
+			}  
+			  
+			public class oddoreven{ 
+		public static void main(String args[]){  
+			Table obj = new Table();//only one object  
+			MyThread1 t1=new MyThread1(obj);  
+			MyThread2 t2=new MyThread2(obj);  
+			t1.start();  
+			t2.start();  
+		}  
+	  
 
 }
